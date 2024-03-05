a6247216f81b6f0c4ba2951db9ce909aef85f971
Noman Nadeem <159953037+GameChangerNoman@users.noreply.github.com>
2024-03-05 14:03:57 +0500

    First JS Task

---
+++ task.js
@@ -0,0 +1,29 @@
+var length = 5;
+var width = 3;
+
+var area =length *width;
+console.log("The area of the rectangle is: " + area);
+
+
+var radius = 5;
+var area = Math.PI * radius * radius;
+console.log("The area of the circle is: " + area);
+
+
+var base = 5;
+var height = 3;
+var area = 0.5 * base * height;
+
+console.log("The area of the triangle is: " + area);
+
+var fahrenheit = 68;
+var celsius = (5/9) * (fahrenheit - 32);
+
+console.log(fahrenheit + "°F is equal to " + celsius.toFixed(2) + "°C");
+
+
+var celsius = 20;
+var fahrenheit = (9/5) * celsius + 32;
+console.log(celsius + "°C is equal to " + fahrenheit.toFixed(2) + "°F");
+
+
